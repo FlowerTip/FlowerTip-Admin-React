5fc3d55af03dc19080d1fdcbd2b19344aae1adb0
aven <17516019587@163.com>
2024-09-13 18:07:31 +0800

    完善信息

--- src/views/cockpit/index.tsx
+++ src/views/cockpit/index.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React from 'react';
+import { Card, Col, Row } from 'antd';
 
-const Cockpit: React.FC = () => {
-  return (
-    <div style={{height: '200px'}}>驾驶舱页面</div>
-  )
-}
+const Cockpit: React.FC = () => (
+  <div style={{ width: 'calc(100vw - 36px)', boxSizing: 'border-box', padding: 0, margin: 0 }}>
+    <Row gutter={10} style={{marginBottom: '10px'}}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+    <Row gutter={10} style={{marginBottom: '10px'}}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+    <Row gutter={10} style={{marginBottom: '10px'}}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false} style={{height: '300px'}}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+  </div>
+);
 
 export default Cockpit;
