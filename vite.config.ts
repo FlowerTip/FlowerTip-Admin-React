dceb0c140e6126f755ec86ed344a9a6fbd61a7c5
aven <17516019587@163.com>
2025-03-19 11:27:27 +0800

    项目开启gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { loadEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteCompression from 'vite-plugin-compression'
+
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     base: "./",
-    plugins: [react()],
+    plugins: [react(), viteCompression({
+      verbose: true, // 是否在控制台输出结果及过程信息
+      disable: false, // 默认不禁止压缩
+      deleteOriginFile: false, // 是否删除源文件
+      threshold: 10240, // 体积大于threshold的文件会被压缩，单位为byte
+      algorithm: 'gzip', // 使用gzip压缩
+      ext: '.gz', // 生成压缩包的扩展名
+    })],
     server: {
       open: true,
       proxy: {
