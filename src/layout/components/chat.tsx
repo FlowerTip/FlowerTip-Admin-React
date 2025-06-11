cff9f132bc540e799d71b539522058f60a1c74c9
aven <17516019587@163.com>
2025-06-11 10:25:35 +0800

    修改网站图标

--- src/layout/components/chat.tsx
+++ src/layout/components/chat.tsx
@@ -296,7 +296,7 @@ const Independent: React.FC = () => {
       header: "狗尾巴花的尖",
       avatar: {
         icon: <img
-          src="./favicon.svg"
+          src="https://www.flowertip.site/upload/admin.png"
           draggable={false}
           alt="logo"
         />, style: { background: '#fff' }
@@ -353,7 +353,7 @@ const Independent: React.FC = () => {
     <Space direction="vertical" align="center" size={16} className={styles.placeholder}>
       <Welcome
         variant="borderless"
-        icon="https://www.flowertip.site/pro/favicon.svg"
+        icon="https://www.flowertip.site/upload/admin.png"
         title="您好, 我是您的智能对话交互助手"
         description="我是狗尾巴花的尖，FlowerTip Admin 智能对话交互助手"
         style={{
